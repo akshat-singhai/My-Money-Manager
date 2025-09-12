407e3f1b4e9d6eec83169836b1c643e81f1849b9
akshat-singhai <akshatsinghai2412@gmail.com>
2025-09-12 17:14:12 +0530

    add some change v5.4.0

--- src/Components/PersonRecords.jsx
+++ src/Components/PersonRecords.jsx
@@ -218,7 +218,7 @@ const PersonRecords = () => {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
           >
-            <h3>{borrowed.length} Udhar Liya</h3>
+            <h3 >{borrowed.length} Udhar Liya</h3>
             <p>{formatINR(borrowedTotal)}</p>
           </motion.div>
 
